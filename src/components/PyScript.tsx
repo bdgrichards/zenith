91dbd12d9dbf2827ac63e2d9e0259a80570fe7a5
Ben Richards <bdgrichards@gmail.com>
2023-09-18 16:25:46 +0100

    Revert "Add pyscript infra"
    This reverts commit 44796169c2fde645b27b15b5b5a40fe8f0efe767.

--- src/components/PyScript.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export class PyScript extends React.Component {
-  render() {
-    // @ts-ignore
-    return React.createElement("py-script", null, this.props.childre);
-  }
-}
