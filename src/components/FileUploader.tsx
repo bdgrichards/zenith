26d696c2bac93b34b63f646c9452913ac7baa049
Ben Richards <bdgrichards@gmail.com>
2023-09-21 12:08:51 +0100

    Add file uploader

---
+++ src/components/FileUploader.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+function CsvUploader() {
+  const [csvData, setCsvData] = useState<string | null>(null);
+
+  const handleFileUpload: React.ChangeEventHandler<HTMLInputElement> = (
+    event
+  ) => {
+    if (event?.target?.files) {
+      const file = event.target.files[0];
+      // Check if the selected file is a CSV file
+      if (
+        (file != null && file.type === "text/csv") ||
+        file?.name.endsWith(".csv")
+      ) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          if (e?.target != null) {
+            // Parse the CSV data
+            const csvContent = e.target.result;
+            // You can save the CSV data to a variable or state
+            setCsvData(String(csvContent));
+          }
+        };
+        reader.readAsText(file);
+      } else {
+        alert("Please select a valid CSV file.");
+      }
+    }
+  };
+
+  return (
+    <div className="w-full shadow-md rounded-lg bg-white mx-auto max-w-md mt-20 pr-4">
+      <input
+        type="file"
+        accept=".csv"
+        onChange={handleFileUpload}
+        className="w-full text-sm text-gray-500
+          file:py-2 file:px-4 file:m-4
+          file:rounded-md file:border-0
+          file:font-semibold file:text-white
+          file:bg-gradient-to-br file:from-pink-500 
+          file:to-yellow-500 file:shadow-none
+          hover:file:shadow-md file:saturate-100 
+          disabled:file:saturate-0 file:transition 
+          file:ease-in-out file:duration-200
+          file:cursor-pointer
+          "
+      />
+    </div>
+  );
+}
+
+export default CsvUploader;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,14 @@
 import Header from "../components/Header";
 import AISuggest from "../components/AISuggest";
 import CodeEditor from "../components/CodeEditor";
+import FileUploader from "../components/FileUploader";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-full bg-slate-100">
       <Header />
       <div className="flex flex-col px-8">
-        <div className="shadow-md rounded-lg bg-white p-4 mx-auto max-w-md mt-20">
-          <div className="text-gray-500">Upload CSV</div>
-        </div>
+        <FileUploader />
         <AISuggest />
         <CodeEditor />
       </div>
