3cc48d28560ce4ac36b0d662aa0c851916118620
Ben Richards <bdgrichards@gmail.com>
2023-09-23 14:26:18 +0100

    Fix file upload styling

--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -56,16 +56,15 @@ function CsvUploader({ setIsCSVUploaded }: Props) {
         accept=".csv"
         onChange={handleFileUpload}
         className="w-full text-sm text-gray-500
-          file:py-2 file:px-4 file:m-4
+          file:py-2 file:px-4 file:mr-4 py-4 pl-4
           file:rounded-md file:border-0
           file:font-semibold file:text-white
-          file:bg-gradient-to-br file:from-pink-500 
+          file:bg-gradient-to-br file:from-pink-500
           file:to-yellow-500 file:shadow-none
-          hover:file:shadow-md file:saturate-100 
-          disabled:file:saturate-0 file:transition 
+          hover:file:shadow-md file:saturate-100
+          disabled:file:saturate-0 file:transition
           file:ease-in-out file:duration-200
-          file:cursor-pointer
-          "
+          file:cursor-pointer"
       />
       {csvData != null && (
         <div className="max-h-64 overflow-auto ml-4 mb-4 rounded-lg outline-1 outline outline-gray-100 shadow-md">
